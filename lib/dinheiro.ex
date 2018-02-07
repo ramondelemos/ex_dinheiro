1083defd76a14311f19fdfb145c0d69305559239
ramondelemos <ramondelemos@gmail.com>
2018-02-07 02:02:52 -0200

    Teste para o deploy automático.

--- lib/dinheiro.ex
+++ lib/dinheiro.ex
@@ -15,4 +15,17 @@ defmodule Dinheiro do
   def hello do
     :world
   end
+
+  @doc """
+  Hello world 2.
+
+  ## Examples
+
+      iex> Dinheiro.hello2
+      :world
+
+  """
+  def hello2 do
+    :world
+  end
 end
