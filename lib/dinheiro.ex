16f2b52d5713b4baf7b5fd19a406171d051caddb
ramondelemos <ramondelemos@gmail.com>
2018-02-06 23:40:03 -0200

    Criação do projeto ex_dinheiro.

---
+++ lib/dinheiro.ex
@@ -0,0 +1,18 @@
+defmodule Dinheiro do
+  @moduledoc """
+  Documentation for Dinheiro.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dinheiro.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dinheiro_test.exs
@@ -0,0 +1,8 @@
+defmodule DinheiroTest do
+  use ExUnit.Case
+  doctest Dinheiro
+
+  test "greets the world" do
+    assert Dinheiro.hello() == :world
+  end
+end
