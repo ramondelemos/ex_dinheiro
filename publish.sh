72602287b307a4f138bf9a0b75f94d10b46a08bb
ramondelemos <ramondelemos@gmail.com>
2018-02-07 23:36:23 -0200

    Instalação do rebar3 no Travis CI.

--- publish.sh
+++ publish.sh
@@ -6,6 +6,9 @@ mkdir -p ~/.hex
 echo '{username,<<"'ramondelemos'">>}.' > ~/.hex/hex.config
 echo '{key,<<"'${HEX_KEY}'">>}.' >> ~/.hex/hex.config
 
-mix hex.publish <<EOF
+mkdir -p ~/.config/rebar3
+echo '{plugins, [rebar3_hex]}.' > ~/.config/rebar3/rebar.config
+
+./rebar3 hex publish <<EOF
 y
 EOF
