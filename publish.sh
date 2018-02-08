ef5e13671a68928dc1e2397e7b6c751b9a695921
ramondelemos <ramondelemos@gmail.com>
2018-02-07 22:43:22 -0200

    Configurando processo de publish no repositório hex.pm.

--- publish.sh
+++ publish.sh
@@ -2,15 +2,6 @@ if [ "$TRAVIS_BRANCH" != "master" ]; then
     exit 0;
 fi
 
-## Setup hex user
-mkdir -p ~/.hex
-echo '{username,<<"'ramondelemos'">>}.' > ~/.hex/hex.config
-echo '{key,<<"'${HEX_KEY}'">>}.' >> ~/.hex/hex.config
-
-## Add the rebar3 hex plugin to global
-mkdir -p ~/.config/rebar3
-echo '{plugins, [rebar3_hex]}.' > ~/.config/rebar3/rebar.config
-
-./rebar3 hex publish <<EOF
-y
-EOF
+mix hex.config username ramondelemos
+mix hex.config key $HEX_KEY
+mix hex.publish
