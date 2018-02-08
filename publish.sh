b5af06c954d24d62b0efdfef9fb041db687c4e3a
ramondelemos <ramondelemos@gmail.com>
2018-02-08 01:26:38 -0200

    Executando hex.publish com arquivo hex.config.

--- publish.sh
+++ publish.sh
@@ -2,13 +2,12 @@ if [ "$TRAVIS_BRANCH" != "master" ]; then
     exit 0;
 fi
 
-mkdir -p ~/.hex
-echo '{username,<<"'ramondelemos'">>}.' > ~/.hex/hex.config
-echo '{key,<<"'${HEX_KEY}'">>}.' >> ~/.hex/hex.config
+echo '{username,<<"'ramondelemos'">>}.' > hex.config
+echo '{key,<<"'${HEX_KEY}'">>}.' >> hex.config
 
 #mkdir -p ~/.config/rebar3
 #echo '{plugins, [rebar3_hex]}.' > ~/.config/rebar3/rebar.config
 
-rebar3 hex publish <<EOF
+mix hex.publish <<EOF
 y
 EOF
