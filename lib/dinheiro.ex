63d9cfcb14255cfe4e4f332519c7137acccd04a1
ramondelemos <ramondelemos@gmail.com>
2018-02-12 19:10:13 -0200

    Remoção dos métodos hello() e hello2().
    Criação do new/1 e new/2.

--- lib/dinheiro.ex
+++ lib/dinheiro.ex
@@ -2,30 +2,55 @@ defmodule Dinheiro do
   @moduledoc """
   Documentation for Dinheiro.
   """
+  defstruct quantia: nil, moeda: nil
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Dinheiro.hello
-      :world
-
+  @typedoc """
+      Type that represents Dinheiro struct with:
+      :quantia as integer
+      :moeda as atom that represents an ISO 4217 code
   """
-  def hello do
-    :world
+  @type t :: %Dinheiro{ quantia: integer, moeda: atom }
+
+  @spec new(integer) :: t
+  @doc """
+  Create a new `Dinheiro` struct using a default value of Moeda.
+  The default currency can be set in the system Mix config.
+  ## Example Config:
+      config :ex_dinheiro, default_moeda: :BRL
+  ## Example:
+      Dinheiro.new(12345)
+      %Dinheiro{ quantia: 12345, moeda: :BRL }
+  """
+  def new(quantia) when is_integer(quantia) do
+    moeda = Application.get_env(:ex_dinheiro, :default_moeda)
+    if moeda do
+      new(quantia, moeda)
+    else
+      raise ArgumentError, "to use Dinheiro.new/1 you must set a default value in your application config :ex_dinheiro, default_moeda."
+    end
   end
 
+  @spec new(integer, atom | String.t) :: t
   @doc """
-  Hello world 2.
-
-  ## Examples
-
-      iex> Dinheiro.hello2
-      :world
+  Create a new `Dinheiro` struct.
+  
+  ## Example:
+      Dinheiro.new(12345, :BRL)
+      %Dinheiro{ quantia: 12345, moeda: :BRL }
+      Dinheiro.new(12345, "BRL")
+      %Dinheiro{ quantia: 12345, moeda: :BRL }
 
   """
-  def hello2 do
-    :world
+  def new(quantia, moeda) when is_integer(quantia) do
+    v_moeda = Moeda.get_atom(moeda);
+    if v_moeda do
+      newp(quantia, v_moeda)
+    else
+      raise ArgumentError, "to use Dinheiro.new/2 you must set a valid value to moeda."
+    end
+  end
+
+  defp newp(quantia, moeda) when is_integer(quantia) and is_atom(moeda) do
+    %Dinheiro{ quantia: quantia, moeda: moeda }
   end
 end
--- test/dinheiro_test.exs
+++ test/dinheiro_test.exs
@@ -2,11 +2,35 @@ defmodule DinheiroTest do
   use ExUnit.Case
   doctest Dinheiro
 
-  test "greets the world" do
-    assert Dinheiro.hello() == :world
+  test "new/1 with default value set" do
+    try do
+      Application.put_env(:ex_dinheiro, :default_moeda, :BRL)
+      assert Dinheiro.new(12345) == Dinheiro.new(12345, :BRL)
+    after
+      Application.delete_env(:ex_dinheiro, :default_moeda)
+    end
   end
 
-  test "greets the world 2" do
-    assert Dinheiro.hello2() == :world
+  test "new/1 with an invalid default value set" do
+    try do
+      Application.put_env(:ex_dinheiro, :default_moeda, :NONE)
+      assert_raise ArgumentError, fn ->
+        Dinheiro.new(12345)
+      end
+    after
+      Application.delete_env(:ex_dinheiro, :default_moeda)
+    end
+  end
+
+  test "new/1 with no config set" do
+    assert_raise ArgumentError, fn ->
+      Dinheiro.new(12345)
+    end
+  end
+
+  test "new/2 requires existing value" do
+    assert_raise ArgumentError, fn ->
+      Dinheiro.new(12345, :NONE)
+    end
   end
 end
