02b5a1923cc6e64a549c13eabd319290006e5854
ramondelemos <ramondelemos@gmail.com>
2018-02-17 16:51:38 -0200

    Refatoração do teste "new/1 with no config set".

--- test/dinheiro_test.exs
+++ test/dinheiro_test.exs
@@ -25,6 +25,7 @@ defmodule DinheiroTest do
 
   test "new/1 with no config set" do
     Application.delete_env(:ex_dinheiro, :default_moeda)
+
     assert_raise ArgumentError, fn ->
       Dinheiro.new(12345)
     end
