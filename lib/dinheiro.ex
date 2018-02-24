581153ca33649926d94515d3a08000be7c7180cb
ramondelemos <ramondelemos@gmail.com>
2018-02-24 11:55:32 -0300

    Refatoração para adequação com as convenções do Elixir.

--- lib/dinheiro.ex
+++ lib/dinheiro.ex
@@ -15,44 +15,44 @@ defmodule Dinheiro do
   """
   @type t :: %Dinheiro{quantia: integer, moeda: atom}
 
-  @spec new(integer | float) :: t
+  @spec new!(integer | float) :: t
   @doc """
   Create a new `Dinheiro` struct using a default currency.
   The default currency can be set in the system Mix config.
 
   ## Example:
         iex> Application.put_env(:ex_dinheiro, :default_currency, :BRL)
-        iex> Dinheiro.new(12345)
+        iex> Dinheiro.new!(12345)
         %Dinheiro{quantia: 1234500, moeda: :BRL}
-        iex> Dinheiro.new(123.45)
+        iex> Dinheiro.new!(123.45)
         %Dinheiro{quantia: 12345, moeda: :BRL}
 
   """
-  def new(quantia) when is_integer(quantia) or is_float(quantia) do
+  def new!(quantia) when is_integer(quantia) or is_float(quantia) do
     moeda = Application.get_env(:ex_dinheiro, :default_currency)
 
     if moeda do
-      new(quantia, moeda)
+      new!(quantia, moeda)
     else
       raise ArgumentError,
-            "to use Dinheiro.new/1 you must set a default value in your application config :ex_dinheiro, default_moeda. #{
+            "to use Dinheiro.new!/1 you must set a default value in your application config :ex_dinheiro, default_moeda. #{
               moeda
             }."
     end
   end
 
-  @spec new(integer | float, atom | String.t()) :: t
+  @spec new!(integer | float, atom | String.t()) :: t
   @doc """
   Create a new `Dinheiro` struct.
 
   ## Example:
-      iex> Dinheiro.new(12345, :BRL)
+      iex> Dinheiro.new!(12345, :BRL)
       %Dinheiro{quantia: 1234500, moeda: :BRL}
-      iex> Dinheiro.new(12345, "BRL")
+      iex> Dinheiro.new!(12345, "BRL")
       %Dinheiro{quantia: 1234500, moeda: :BRL}
-      iex> Dinheiro.new(123.45, :BRL)
+      iex> Dinheiro.new!(123.45, :BRL)
       %Dinheiro{quantia: 12345, moeda: :BRL}
-      iex> Dinheiro.new(123.45, "BRL")
+      iex> Dinheiro.new!(123.45, "BRL")
       %Dinheiro{quantia: 12345, moeda: :BRL}
 
   Is possible to work with no official ISO currency code adding it in the system Mix config.
@@ -63,11 +63,11 @@ defmodule Dinheiro do
       nil
       iex> moedas = %{ XBT: %Moeda{nome: "Bitcoin", simbolo: '฿', codigo: "XBT", codigo_iso: 0, expoente: 8} }
       iex> Application.put_env(:ex_dinheiro, :unofficial_currencies, moedas)
-      iex> Dinheiro.new(123.45, :XBT)
+      iex> Dinheiro.new!(123.45, :XBT)
       %Dinheiro{quantia: 12345000000, moeda: :XBT}
 
   """
-  def new(quantia, moeda) when is_integer(quantia) or is_float(quantia) do
+  def new!(quantia, moeda) when is_integer(quantia) or is_float(quantia) do
     v_moeda = Moeda.find(moeda)
 
     if v_moeda do
@@ -86,7 +86,7 @@ defmodule Dinheiro do
       |> do_new(atom)
     else
       raise ArgumentError,
-            "to use Dinheiro.new/2 you must set a valid value to moeda."
+            "to use Dinheiro.new!/2 you must set a valid value to moeda."
     end
   end
 
@@ -94,20 +94,20 @@ defmodule Dinheiro do
     %Dinheiro{quantia: quantia, moeda: moeda}
   end
 
-  @spec compare(t, t) :: integer
+  @spec compare!(t, t) :: integer
   @doc """
   Compares two `Dinheiro` structs with each other.
   They must each be of the same moeda and then their value are compared.
 
   ## Example:
-      iex> Dinheiro.compare(Dinheiro.new(12345, :BRL), Dinheiro.new(12345, :BRL))
+      iex> Dinheiro.compare!(Dinheiro.new!(12345, :BRL), Dinheiro.new!(12345, :BRL))
       0
-      iex> Dinheiro.compare(Dinheiro.new(12345, :BRL), Dinheiro.new(12346, :BRL))
+      iex> Dinheiro.compare!(Dinheiro.new!(12345, :BRL), Dinheiro.new!(12346, :BRL))
       -1
-      iex> Dinheiro.compare(Dinheiro.new(12346, :BRL), Dinheiro.new(12345, :BRL))
+      iex> Dinheiro.compare!(Dinheiro.new!(12346, :BRL), Dinheiro.new!(12345, :BRL))
       1
   """
-  def compare(%Dinheiro{moeda: m} = a, %Dinheiro{moeda: m} = b) do
+  def compare!(%Dinheiro{moeda: m} = a, %Dinheiro{moeda: m} = b) do
     case a.quantia - b.quantia do
       result when result > 0 -> 1
       result when result < 0 -> -1
@@ -115,7 +115,7 @@ defmodule Dinheiro do
     end
   end
 
-  def compare(a, b) do
+  def compare!(a, b) do
     raise_moeda_must_be_the_same(a, b)
   end
 
@@ -124,11 +124,11 @@ defmodule Dinheiro do
   Retun `true` if two `Dinheiro` structs are equals.
 
   ## Example:
-      iex> Dinheiro.equals?(Dinheiro.new(12345, :BRL), Dinheiro.new(12345, :BRL))
+      iex> Dinheiro.equals?(Dinheiro.new!(12345, :BRL), Dinheiro.new!(12345, :BRL))
       true
-      iex> Dinheiro.equals?(Dinheiro.new(12345, :BRL), Dinheiro.new(12346, :BRL))
+      iex> Dinheiro.equals?(Dinheiro.new!(12345, :BRL), Dinheiro.new!(12346, :BRL))
       false
-      iex> Dinheiro.equals?(Dinheiro.new(12345, :BRL), Dinheiro.new(12345, :USD))
+      iex> Dinheiro.equals?(Dinheiro.new!(12345, :BRL), Dinheiro.new!(12345, :USD))
       false
   """
   def equals?(%Dinheiro{moeda: moeda, quantia: quantia}, %Dinheiro{
@@ -139,106 +139,106 @@ defmodule Dinheiro do
 
   def equals?(a, b), do: false
 
-  @spec sum(t, t | integer | float) :: t
+  @spec sum!(t, t | integer | float) :: t
   @doc """
   Return a new `Dinheiro` structs with sum of two values.
   The first parameter must be a struct of `Dinheiro`.
 
   ## Example:
-      iex> Dinheiro.sum(Dinheiro.new(1, :BRL), Dinheiro.new(1, :BRL))
+      iex> Dinheiro.sum!(Dinheiro.new!(1, :BRL), Dinheiro.new!(1, :BRL))
       %Dinheiro{quantia: 200, moeda: :BRL}
-      iex> Dinheiro.sum(Dinheiro.new(1, :BRL), 2)
+      iex> Dinheiro.sum!(Dinheiro.new!(1, :BRL), 2)
       %Dinheiro{quantia: 300, moeda: :BRL}
-      iex> Dinheiro.sum(Dinheiro.new(1, :BRL), 2.5)
+      iex> Dinheiro.sum!(Dinheiro.new!(1, :BRL), 2.5)
       %Dinheiro{quantia: 350, moeda: :BRL}
-      iex> Dinheiro.sum(Dinheiro.new(2, :BRL), -1)
+      iex> Dinheiro.sum!(Dinheiro.new!(2, :BRL), -1)
       %Dinheiro{quantia: 100, moeda: :BRL}
 
   """
-  def sum(%Dinheiro{moeda: m} = a, %Dinheiro{moeda: m} = b) do
+  def sum!(%Dinheiro{moeda: m} = a, %Dinheiro{moeda: m} = b) do
     %Dinheiro{quantia: a.quantia + b.quantia, moeda: m}
   end
 
-  def sum(%Dinheiro{moeda: m} = a, b) when is_integer(b) or is_float(b) do
-    sum(a, Dinheiro.new(b, m))
+  def sum!(%Dinheiro{moeda: m} = a, b) when is_integer(b) or is_float(b) do
+    sum!(a, Dinheiro.new!(b, m))
   end
 
-  def sum(a, b) do
+  def sum!(a, b) do
     raise_moeda_must_be_the_same(a, b)
   end
 
-  @spec subtract(t, t | integer | float) :: t
+  @spec subtract!(t, t | integer | float) :: t
   @doc """
   Return a new `Dinheiro` structs with subtract of two values.
   The first parameter must be a struct of `Dinheiro`.
 
   ## Example:
-      iex> Dinheiro.subtract(Dinheiro.new(2, :BRL), Dinheiro.new(1, :BRL))
+      iex> Dinheiro.subtract!(Dinheiro.new!(2, :BRL), Dinheiro.new!(1, :BRL))
       %Dinheiro{quantia: 100, moeda: :BRL}
-      iex> Dinheiro.subtract(Dinheiro.new(4, :BRL), 2)
+      iex> Dinheiro.subtract!(Dinheiro.new!(4, :BRL), 2)
       %Dinheiro{quantia: 200, moeda: :BRL}
-      iex> Dinheiro.subtract(Dinheiro.new(5, :BRL), 2.5)
+      iex> Dinheiro.subtract!(Dinheiro.new!(5, :BRL), 2.5)
       %Dinheiro{quantia: 250, moeda: :BRL}
-      iex> Dinheiro.subtract(Dinheiro.new(4, :BRL), -2)
+      iex> Dinheiro.subtract!(Dinheiro.new!(4, :BRL), -2)
       %Dinheiro{quantia: 600, moeda: :BRL}
 
   """
-  def subtract(%Dinheiro{moeda: m} = a, %Dinheiro{moeda: m} = b) do
+  def subtract!(%Dinheiro{moeda: m} = a, %Dinheiro{moeda: m} = b) do
     %Dinheiro{quantia: a.quantia - b.quantia, moeda: m}
   end
 
-  def subtract(%Dinheiro{moeda: m} = a, b) when is_integer(b) or is_float(b) do
-    subtract(a, Dinheiro.new(b, m))
+  def subtract!(%Dinheiro{moeda: m} = a, b) when is_integer(b) or is_float(b) do
+    subtract!(a, Dinheiro.new!(b, m))
   end
 
-  def subtract(a, b) do
+  def subtract!(a, b) do
     raise_moeda_must_be_the_same(a, b)
   end
 
-  @spec multiply(t, integer | float) :: t
+  @spec multiply!(t, integer | float) :: t
   @doc """
   Return a new `Dinheiro` structs with value multiplied by other value.
   The first parameter must be a struct of `Dinheiro`.
 
   ## Example:
-      iex> Dinheiro.multiply(Dinheiro.new(2, :BRL), 2)
+      iex> Dinheiro.multiply!(Dinheiro.new!(2, :BRL), 2)
       %Dinheiro{quantia: 400, moeda: :BRL}
-      iex> Dinheiro.multiply(Dinheiro.new(5, :BRL), 2.5)
+      iex> Dinheiro.multiply!(Dinheiro.new!(5, :BRL), 2.5)
       %Dinheiro{quantia: 1250, moeda: :BRL}
-      iex> Dinheiro.multiply(Dinheiro.new(4, :BRL), -2)
+      iex> Dinheiro.multiply!(Dinheiro.new!(4, :BRL), -2)
       %Dinheiro{quantia: -800, moeda: :BRL}
 
   """
-  def multiply(%Dinheiro{moeda: m} = a, b) when is_integer(b) or is_float(b) do
-    float_value = to_float(a)
-    new(float_value * b, m)
+  def multiply!(%Dinheiro{moeda: m} = a, b) when is_integer(b) or is_float(b) do
+    float_value = to_float!(a)
+    new!(float_value * b, m)
   end
 
-  @spec divide(t, integer | [integer]) :: [t]
+  @spec divide!(t, integer | [integer]) :: [t]
   @doc """
   Divide a `Dinheiro` struct by a positive integer value
 
   ## Example:
-      iex> Dinheiro.divide(Dinheiro.new(100, :BRL), 2)
+      iex> Dinheiro.divide!(Dinheiro.new!(100, :BRL), 2)
       [%Dinheiro{quantia: 5000, moeda: :BRL}, %Dinheiro{quantia: 5000, moeda: :BRL}]
-      iex> Dinheiro.divide(Dinheiro.new(101, :BRL), 2)
+      iex> Dinheiro.divide!(Dinheiro.new!(101, :BRL), 2)
       [%Dinheiro{quantia: 5050, moeda: :BRL}, %Dinheiro{quantia: 5050, moeda: :BRL}]
 
   Divide a `Dinheiro` struct by an list of values that represents a division ratio.
 
   ## Example:
-      iex> Dinheiro.divide(Dinheiro.new(0.05, :BRL), [3, 7])
+      iex> Dinheiro.divide!(Dinheiro.new!(0.05, :BRL), [3, 7])
       [%Dinheiro{quantia: 2, moeda: :BRL}, %Dinheiro{quantia: 3, moeda: :BRL}]
 
   """
-  def divide(%Dinheiro{moeda: m} = a, b) when is_integer(b) do
+  def divide!(%Dinheiro{moeda: m} = a, b) when is_integer(b) do
     assert_if_ratios_are_valid([b])
     division = div(a.quantia, b)
     remainder = rem(a.quantia, b)
     to_alocate(division, remainder, m, b)
   end
 
-  def divide(%Dinheiro{moeda: m} = a, b) when is_list(b) do
+  def divide!(%Dinheiro{moeda: m} = a, b) when is_list(b) do
     assert_if_ratios_are_valid(b)
     ratio = sum_values(b)
     division = calculate_ratio(b, ratio, a.quantia)
@@ -301,20 +301,20 @@ defmodule Dinheiro do
   defp sum_values([]), do: 0
   defp sum_values([head | tail]), do: head + sum_values(tail)
 
-  @spec to_float(t) :: float
+  @spec to_float!(t) :: float
   @doc """
   Return a float value from a `Dinheiro` structs.
 
   ## Example:
-      iex> Dinheiro.to_float(%Dinheiro{quantia: 200, moeda: :BRL})
+      iex> Dinheiro.to_float!(%Dinheiro{quantia: 200, moeda: :BRL})
       2.0
-      iex> Dinheiro.to_float(Dinheiro.new(50.5, :BRL))
+      iex> Dinheiro.to_float!(Dinheiro.new!(50.5, :BRL))
       50.5
-      iex> Dinheiro.to_float(Dinheiro.new(-4, :BRL))
+      iex> Dinheiro.to_float!(Dinheiro.new!(-4, :BRL))
       -4.0
 
   """
-  def to_float(%Dinheiro{moeda: m} = from) do
+  def to_float!(%Dinheiro{moeda: m} = from) do
     moeda = Moeda.find(m)
 
     unless moeda,
@@ -335,9 +335,9 @@ defmodule Dinheiro do
   ## Example:
       iex> Dinheiro.to_string!(%Dinheiro{quantia: 200, moeda: :BRL})
       "R$ 2,00"
-      iex> Dinheiro.to_string!(Dinheiro.new(50.5, :BRL))
+      iex> Dinheiro.to_string!(Dinheiro.new!(50.5, :BRL))
       "R$ 50,50"
-      iex> Dinheiro.to_string!(Dinheiro.new(-4, :BRL))
+      iex> Dinheiro.to_string!(Dinheiro.new!(-4, :BRL))
       "R$ -4,00"
 
   Using options-style parameters you can change the behavior of the function.
@@ -349,13 +349,13 @@ defmodule Dinheiro do
 
   ## Exemples
 
-      iex> Dinheiro.to_string!(Dinheiro.new(1000.5, :USD), thousand_separator: ",", decimal_separator: ".")
+      iex> Dinheiro.to_string!(Dinheiro.new!(1000.5, :USD), thousand_separator: ",", decimal_separator: ".")
       "$ 1,000.50"
-      iex> Dinheiro.to_string!(Dinheiro.new(1000.5, :USD), display_currency_symbol: false)
+      iex> Dinheiro.to_string!(Dinheiro.new!(1000.5, :USD), display_currency_symbol: false)
       "1.000,50"
-      iex> Dinheiro.to_string!(Dinheiro.new(1000.5, :USD), display_currency_code: true)
+      iex> Dinheiro.to_string!(Dinheiro.new!(1000.5, :USD), display_currency_code: true)
       "$ 1.000,50 USD"
-      iex> Dinheiro.to_string!(Dinheiro.new(1000.5, :USD), display_currency_code: true, display_currency_symbol: false)
+      iex> Dinheiro.to_string!(Dinheiro.new!(1000.5, :USD), display_currency_code: true, display_currency_symbol: false)
       "1.000,50 USD"
 
   The default values also can be set in the system Mix config.
@@ -363,13 +363,13 @@ defmodule Dinheiro do
   ## Example:
       iex> Application.put_env(:ex_dinheiro, :thousand_separator, ",")
       iex> Application.put_env(:ex_dinheiro, :decimal_separator, ".")
-      iex> Dinheiro.to_string!(Dinheiro.new(1000.5, :USD))
+      iex> Dinheiro.to_string!(Dinheiro.new!(1000.5, :USD))
       "$ 1,000.50"
       iex> Application.put_env(:ex_dinheiro, :display_currency_symbol, false)
-      iex> Dinheiro.to_string!(Dinheiro.new(5000.5, :USD))
+      iex> Dinheiro.to_string!(Dinheiro.new!(5000.5, :USD))
       "5,000.50"
       iex> Application.put_env(:ex_dinheiro, :display_currency_code, true)
-      iex> Dinheiro.to_string!(Dinheiro.new(10000.0, :USD))
+      iex> Dinheiro.to_string!(Dinheiro.new!(10000.0, :USD))
       "10,000.00 USD"
 
   The options-style parameters override values in the system Mix config.
@@ -377,21 +377,21 @@ defmodule Dinheiro do
   ## Example:
       iex> Application.put_env(:ex_dinheiro, :thousand_separator, ",")
       iex> Application.put_env(:ex_dinheiro, :decimal_separator, ".")
-      iex> Dinheiro.to_string!(Dinheiro.new(1000.5, :USD))
+      iex> Dinheiro.to_string!(Dinheiro.new!(1000.5, :USD))
       "$ 1,000.50"
-      iex> Dinheiro.to_string!(Dinheiro.new(1000.5, :BRL), thousand_separator: ".", decimal_separator: ",")
+      iex> Dinheiro.to_string!(Dinheiro.new!(1000.5, :BRL), thousand_separator: ".", decimal_separator: ",")
       "R$ 1.000,50"
 
   Is possible to override some official ISO currency code adding it in the system Mix config.
 
   ## Examples
 
-      iex> Dinheiro.to_string!(Dinheiro.new(12_345_678.9, :BRL))
+      iex> Dinheiro.to_string!(Dinheiro.new!(12_345_678.9, :BRL))
       "R$ 12.345.678,90"
-      iex> Dinheiro.to_string!(Dinheiro.new(12_345_678.9, :USD))
+      iex> Dinheiro.to_string!(Dinheiro.new!(12_345_678.9, :USD))
       "$ 12.345.678,90"
-      iex> Dinheiro.to_string!(Dinheiro.new(12_345_678.9, :XBT))
-      ** (ArgumentError) to use Dinheiro.new/2 you must set a valid value to moeda.
+      iex> Dinheiro.to_string!(Dinheiro.new!(12_345_678.9, :XBT))
+      ** (ArgumentError) to use Dinheiro.new!/2 you must set a valid value to moeda.
       iex> real = %Moeda{nome: "Moeda do Brasil", simbolo: 'BR$', codigo: "BRL", codigo_iso: 986, expoente: 4}
       %Moeda{nome: "Moeda do Brasil", simbolo: 'BR$', codigo: "BRL", codigo_iso: 986, expoente: 4}
       iex> dollar = %Moeda{nome: "Moeda do EUA", simbolo: 'US$', codigo: "USD", codigo_iso: 840, expoente: 3}
@@ -400,16 +400,16 @@ defmodule Dinheiro do
       %Moeda{nome: "Bitcoin", simbolo: '฿', codigo: "XBT", codigo_iso: 0, expoente: 8}
       iex> moedas = %{ BRL: real, USD: dollar, XBT: bitcoin }
       iex> Application.put_env(:ex_dinheiro, :unofficial_currencies, moedas)
-      iex> Dinheiro.to_string!(Dinheiro.new(12_345_678.9, :BRL))
+      iex> Dinheiro.to_string!(Dinheiro.new!(12_345_678.9, :BRL))
       "BR$ 12.345.678,9000"
-      iex> Dinheiro.to_string!(Dinheiro.new(12_345_678.9, :usd))
+      iex> Dinheiro.to_string!(Dinheiro.new!(12_345_678.9, :usd))
       "US$ 12.345.678,900"
-      iex> Dinheiro.to_string!(Dinheiro.new(12_345_678.9, "XBT"))
+      iex> Dinheiro.to_string!(Dinheiro.new!(12_345_678.9, "XBT"))
       "฿ 12.345.678,90000000"
 
   """
   def to_string!(%Dinheiro{moeda: m} = from, opts \\ []) do
-    value = to_float(from)
+    value = to_float!(from)
     Moeda.to_string!(m, value, opts)
   end
 
--- test/dinheiro_test.exs
+++ test/dinheiro_test.exs
@@ -11,76 +11,76 @@ defmodule DinheiroTest do
     Application.delete_env(:ex_dinheiro, :unofficial_currencies)
   end
 
-  test "new/1 with default value set" do
+  test "new!/1 with default value set" do
     Application.put_env(:ex_dinheiro, :default_currency, :BRL)
 
-    assert Dinheiro.new(12345) == %Dinheiro{quantia: 1_234_500, moeda: :BRL}
+    assert Dinheiro.new!(12345) == %Dinheiro{quantia: 1_234_500, moeda: :BRL}
   end
 
-  test "new/1 with an invalid default value set" do
+  test "new!/1 with an invalid default value set" do
     Application.put_env(:ex_dinheiro, :default_currency, :NONE)
 
     assert_raise ArgumentError, fn ->
-      Dinheiro.new(12345)
+      Dinheiro.new!(12345)
     end
   end
 
-  test "new/1 with no config set" do
+  test "new!/1 with no config set" do
     Application.delete_env(:ex_dinheiro, :default_currency)
 
     assert_raise ArgumentError, fn ->
-      Dinheiro.new(12345)
+      Dinheiro.new!(12345)
     end
   end
 
-  test "new/2 requires existing value" do
+  test "new!/2 requires existing value" do
     assert_raise ArgumentError, fn ->
-      Dinheiro.new(12345, :NONE)
+      Dinheiro.new!(12345, :NONE)
     end
   end
 
-  test "new/1 with float value" do
+  test "new!/1 with float value" do
     Application.put_env(:ex_dinheiro, :default_currency, :BRL)
 
-    assert Dinheiro.new(123.45) == %Dinheiro{quantia: 12345, moeda: :BRL}
+    assert Dinheiro.new!(123.45) == %Dinheiro{quantia: 12345, moeda: :BRL}
   end
 
-  test "new/1 with an invalid value" do
+  test "new!/1 with an invalid value" do
     Application.put_env(:ex_dinheiro, :default_currency, :BRL)
 
     assert_raise FunctionClauseError, fn ->
-      Dinheiro.new("1234")
+      Dinheiro.new!("1234")
     end
   end
 
-  test "new/2 with a float value" do
-    assert Dinheiro.new(123.45, :BRL) == %Dinheiro{quantia: 12345, moeda: :BRL}
+  test "new!/2 with a float value" do
+    assert Dinheiro.new!(123.45, :BRL) == %Dinheiro{quantia: 12345, moeda: :BRL}
   end
 
-  test "new/2 with an invalid value" do
+  test "new!/2 with an invalid value" do
     assert_raise FunctionClauseError, fn ->
-      Dinheiro.new("12345", :BRL)
+      Dinheiro.new!("12345", :BRL)
     end
   end
 
-  test "compare/1" do
-    assert Dinheiro.compare(
-             Dinheiro.new(123.45, :BRL),
-             Dinheiro.new(123.45, :BRL)
+  test "compare!(1" do
+    assert Dinheiro.compare!(
+             Dinheiro.new!(123.45, :BRL),
+             Dinheiro.new!(123.45, :BRL)
            ) == 0
 
-    assert Dinheiro.compare(
-             Dinheiro.new(123.45, :BRL),
-             Dinheiro.new(123.46, :BRL)
+    assert Dinheiro.compare!(
+             Dinheiro.new!(123.45, :BRL),
+             Dinheiro.new!(123.46, :BRL)
            ) == -1
 
-    assert Dinheiro.compare(
-             Dinheiro.new(123.46, :BRL),
-             Dinheiro.new(123.45, :BRL)
+    assert Dinheiro.compare!(
+             Dinheiro.new!(123.46, :BRL),
+             Dinheiro.new!(123.45, :BRL)
            ) == 1
 
     assert_raise ArgumentError, fn ->
-      Dinheiro.compare(Dinheiro.new(123.45, :BRL), %Dinheiro{
+      Dinheiro.compare!(Dinheiro.new!(123.45, :BRL), %Dinheiro{
         quantia: 12345,
         moeda: :USD
       }) == 0
@@ -89,181 +89,181 @@ defmodule DinheiroTest do
 
   test "equals?/2" do
     assert Dinheiro.equals?(
-             Dinheiro.new(123.45, :BRL),
-             Dinheiro.new(123.45, :BRL)
+             Dinheiro.new!(123.45, :BRL),
+             Dinheiro.new!(123.45, :BRL)
            ) == true
 
     assert Dinheiro.equals?(
-             Dinheiro.new(123.45, :BRL),
-             Dinheiro.new(123.46, :BRL)
+             Dinheiro.new!(123.45, :BRL),
+             Dinheiro.new!(123.46, :BRL)
            ) == false
 
     assert Dinheiro.equals?(
-             Dinheiro.new(123.46, :BRL),
-             Dinheiro.new(123.46, :USD)
+             Dinheiro.new!(123.46, :BRL),
+             Dinheiro.new!(123.46, :USD)
            ) == false
   end
 
-  test "sum/2" do
-    assert Dinheiro.sum(Dinheiro.new(12345, :BRL), Dinheiro.new(12345, :BRL)) ==
+  test "sum!(2" do
+    assert Dinheiro.sum!(Dinheiro.new!(12345, :BRL), Dinheiro.new!(12345, :BRL)) ==
              %Dinheiro{
                quantia: 2_469_000,
                moeda: :BRL
              }
 
-    assert Dinheiro.sum(Dinheiro.new(1, :BRL), 1) == %Dinheiro{
+    assert Dinheiro.sum!(Dinheiro.new!(1, :BRL), 1) == %Dinheiro{
              quantia: 200,
              moeda: :BRL
            }
 
-    assert Dinheiro.sum(Dinheiro.new(1, :BRL), 1.2) == %Dinheiro{
+    assert Dinheiro.sum!(Dinheiro.new!(1, :BRL), 1.2) == %Dinheiro{
              quantia: 220,
              moeda: :BRL
            }
 
-    assert Dinheiro.sum(Dinheiro.new(-1, :BRL), 1.2) == %Dinheiro{
+    assert Dinheiro.sum!(Dinheiro.new!(-1, :BRL), 1.2) == %Dinheiro{
              quantia: 20,
              moeda: :BRL
            }
 
     assert_raise ArgumentError, fn ->
-      Dinheiro.sum(Dinheiro.new(1, :BRL), Dinheiro.new(1, :USD))
+      Dinheiro.sum!(Dinheiro.new!(1, :BRL), Dinheiro.new!(1, :USD))
     end
   end
 
-  test "subtract/2" do
-    assert Dinheiro.subtract(Dinheiro.new(2, :BRL), Dinheiro.new(1, :BRL)) ==
+  test "subtract!(2" do
+    assert Dinheiro.subtract!(Dinheiro.new!(2, :BRL), Dinheiro.new!(1, :BRL)) ==
              %Dinheiro{
                quantia: 100,
                moeda: :BRL
              }
 
-    assert Dinheiro.subtract(Dinheiro.new(3, :BRL), 1) == %Dinheiro{
+    assert Dinheiro.subtract!(Dinheiro.new!(3, :BRL), 1) == %Dinheiro{
              quantia: 200,
              moeda: :BRL
            }
 
-    assert Dinheiro.subtract(Dinheiro.new(4, :BRL), 1.2) == %Dinheiro{
+    assert Dinheiro.subtract!(Dinheiro.new!(4, :BRL), 1.2) == %Dinheiro{
              quantia: 280,
              moeda: :BRL
            }
 
-    assert Dinheiro.subtract(Dinheiro.new(1, :BRL), -1) == %Dinheiro{
+    assert Dinheiro.subtract!(Dinheiro.new!(1, :BRL), -1) == %Dinheiro{
              quantia: 200,
              moeda: :BRL
            }
 
     assert_raise ArgumentError, fn ->
-      Dinheiro.subtract(Dinheiro.new(2, :BRL), Dinheiro.new(1, :USD))
+      Dinheiro.subtract!(Dinheiro.new!(2, :BRL), Dinheiro.new!(1, :USD))
     end
   end
 
-  test "multiply/2" do
-    assert Dinheiro.multiply(Dinheiro.new(3, :BRL), 2) == %Dinheiro{
+  test "multiply!(2" do
+    assert Dinheiro.multiply!(Dinheiro.new!(3, :BRL), 2) == %Dinheiro{
              quantia: 600,
              moeda: :BRL
            }
 
-    assert Dinheiro.multiply(Dinheiro.new(4, :BRL), 1.5) == %Dinheiro{
+    assert Dinheiro.multiply!(Dinheiro.new!(4, :BRL), 1.5) == %Dinheiro{
              quantia: 600,
              moeda: :BRL
            }
 
-    assert Dinheiro.multiply(Dinheiro.new(1, :BRL), -1) == %Dinheiro{
+    assert Dinheiro.multiply!(Dinheiro.new!(1, :BRL), -1) == %Dinheiro{
              quantia: -100,
              moeda: :BRL
            }
 
     assert_raise FunctionClauseError, fn ->
-      Dinheiro.multiply(2, 2)
+      Dinheiro.multiply!(2, 2)
     end
 
     assert_raise FunctionClauseError, fn ->
-      Dinheiro.multiply(%{quantia: 600, moeda: :BRL}, 2)
+      Dinheiro.multiply!(%{quantia: 600, moeda: :BRL}, 2)
     end
   end
 
-  test "to_float/1" do
-    assert Dinheiro.to_float(%Dinheiro{quantia: 600, moeda: :BRL}) == 6.0
-    assert Dinheiro.to_float(%Dinheiro{quantia: 625, moeda: :BRL}) == 6.25
-    assert Dinheiro.to_float(%Dinheiro{quantia: -625, moeda: :BRL}) == -6.25
+  test "to_float!(1" do
+    assert Dinheiro.to_float!(%Dinheiro{quantia: 600, moeda: :BRL}) == 6.0
+    assert Dinheiro.to_float!(%Dinheiro{quantia: 625, moeda: :BRL}) == 6.25
+    assert Dinheiro.to_float!(%Dinheiro{quantia: -625, moeda: :BRL}) == -6.25
 
     assert_raise FunctionClauseError, fn ->
-      Dinheiro.to_float(%{quantia: 600, moeda: :BRL})
+      Dinheiro.to_float!(%{quantia: 600, moeda: :BRL})
     end
 
     assert_raise FunctionClauseError, fn ->
-      Dinheiro.to_float(123)
+      Dinheiro.to_float!(123)
     end
 
     assert_raise ArgumentError, fn ->
-      Dinheiro.to_float(%Dinheiro{quantia: 600, moeda: :NONE})
+      Dinheiro.to_float!(%Dinheiro{quantia: 600, moeda: :NONE})
     end
   end
 
-  test "divide/2" do
-    assert Dinheiro.divide(Dinheiro.new(0.02, :BRL), 3) == [
+  test "divide!(2" do
+    assert Dinheiro.divide!(Dinheiro.new!(0.02, :BRL), 3) == [
              %Dinheiro{quantia: 1, moeda: :BRL},
              %Dinheiro{quantia: 1, moeda: :BRL},
              %Dinheiro{quantia: 0, moeda: :BRL}
            ]
 
-    assert Dinheiro.divide(%Dinheiro{quantia: 600, moeda: :BRL}, 2) == [
+    assert Dinheiro.divide!(%Dinheiro{quantia: 600, moeda: :BRL}, 2) == [
              %Dinheiro{quantia: 300, moeda: :BRL},
              %Dinheiro{quantia: 300, moeda: :BRL}
            ]
 
-    assert Dinheiro.divide(%Dinheiro{quantia: 625, moeda: :BRL}, 2) == [
+    assert Dinheiro.divide!(%Dinheiro{quantia: 625, moeda: :BRL}, 2) == [
              %Dinheiro{quantia: 313, moeda: :BRL},
              %Dinheiro{quantia: 312, moeda: :BRL}
            ]
 
-    assert Dinheiro.divide(Dinheiro.new(0.05, :BRL), [3, 7]) == [
+    assert Dinheiro.divide!(Dinheiro.new!(0.05, :BRL), [3, 7]) == [
              %Dinheiro{quantia: 2, moeda: :BRL},
              %Dinheiro{quantia: 3, moeda: :BRL}
            ]
 
     assert_raise ArgumentError, fn ->
-      Dinheiro.divide(%Dinheiro{quantia: 600, moeda: :BRL}, 0)
+      Dinheiro.divide!(%Dinheiro{quantia: 600, moeda: :BRL}, 0)
     end
 
     assert_raise ArgumentError, fn ->
-      Dinheiro.divide(%Dinheiro{quantia: 600, moeda: :BRL}, -1)
+      Dinheiro.divide!(%Dinheiro{quantia: 600, moeda: :BRL}, -1)
     end
 
     assert_raise ArgumentError, fn ->
-      Dinheiro.divide(%Dinheiro{quantia: 600, moeda: :BRL}, [-1])
+      Dinheiro.divide!(%Dinheiro{quantia: 600, moeda: :BRL}, [-1])
     end
 
     assert_raise ArgumentError, fn ->
-      Dinheiro.divide(%Dinheiro{quantia: 600, moeda: :BRL}, [""])
+      Dinheiro.divide!(%Dinheiro{quantia: 600, moeda: :BRL}, [""])
     end
 
     assert_raise ArgumentError, fn ->
-      Dinheiro.divide(%Dinheiro{quantia: 600, moeda: :BRL}, [1.0])
+      Dinheiro.divide!(%Dinheiro{quantia: 600, moeda: :BRL}, [1.0])
     end
 
     assert_raise FunctionClauseError, fn ->
-      Dinheiro.divide(%Dinheiro{quantia: 600, moeda: :BRL}, 1.0)
+      Dinheiro.divide!(%Dinheiro{quantia: 600, moeda: :BRL}, 1.0)
     end
 
     assert_raise FunctionClauseError, fn ->
-      Dinheiro.divide(%{quantia: 600, moeda: :BRL}, 2)
+      Dinheiro.divide!(%{quantia: 600, moeda: :BRL}, 2)
     end
   end
 
-  test "to_string/2" do
-    assert Dinheiro.to_string!(Dinheiro.new(0.1, :BRL)) == "R$ 0,10"
-    assert Dinheiro.to_string!(Dinheiro.new(1.0, "BRL")) == "R$ 1,00"
-    assert Dinheiro.to_string!(Dinheiro.new(10.0, :brl)) == "R$ 10,00"
-    assert Dinheiro.to_string!(Dinheiro.new(100.0, "brl")) == "R$ 100,00"
-    assert Dinheiro.to_string!(Dinheiro.new(-1000.0, :BRL)) == "R$ -1.000,00"
+  test "to_string!/2" do
+    assert Dinheiro.to_string!(Dinheiro.new!(0.1, :BRL)) == "R$ 0,10"
+    assert Dinheiro.to_string!(Dinheiro.new!(1.0, "BRL")) == "R$ 1,00"
+    assert Dinheiro.to_string!(Dinheiro.new!(10.0, :brl)) == "R$ 10,00"
+    assert Dinheiro.to_string!(Dinheiro.new!(100.0, "brl")) == "R$ 100,00"
+    assert Dinheiro.to_string!(Dinheiro.new!(-1000.0, :BRL)) == "R$ -1.000,00"
 
-    assert Dinheiro.to_string!(Dinheiro.new(12_345_678.9, :BRL)) ==
+    assert Dinheiro.to_string!(Dinheiro.new!(12_345_678.9, :BRL)) ==
              "R$ 12.345.678,90"
 
     assert Dinheiro.to_string!(
-             Dinheiro.new(12_345_678.9, :USD),
+             Dinheiro.new!(12_345_678.9, :USD),
              thousand_separator: ",",
              decimal_separator: "."
            ) == "$ 12,345,678.90"
